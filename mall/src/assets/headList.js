da9613bbaa3a4a81e90dd5d44da5cdfe846562ac
zw <zzw1366@163.com>
2020-03-11 17:26:20 +0800

    增加头像‘

--- mall/src/assets/headList.js
+++ mall/src/assets/headList.js
@@ -1,22 +1,82 @@
 export const headList = [
   {
     id: 1,
-    url: 'http://106.13.204.211/headList/KCJP6pAR.webp'
+    url: 'http://106.13.204.211/headList/1.jpg'
+  },
+  {
+    id: 2,
+    url: 'http://106.13.204.211/headList/2.jpg'
+  },
+  {
+    id: 3,
+    url: 'http://106.13.204.211/headList/3.jpg'
+  },
+  {
+    id: 4,
+    url: 'http://106.13.204.211/headList/4.jpg'
+  },
+  {
+    id: 5,
+    url: 'http://106.13.204.211/headList/5.jpg'
+  },
+  {
+    id: 6,
+    url: 'http://106.13.204.211/headList/6.jpg'
+  },
+  {
+    id: 7,
+    url: 'http://106.13.204.211/headList/7.jpg'
+  },
+  {
+    id: 8,
+    url: 'http://106.13.204.211/headList/8.jpg'
+  },
+  {
+    id: 9,
+    url: 'http://106.13.204.211/headList/9.jpg'
+  },
+  {
+    id: 10,
+    url: 'http://106.13.204.211/headList/10.jpg'
   },
   {
     id: 11,
-    url: 'http://106.13.204.211/headList/1-160430203151.jpg'
+    url: 'http://106.13.204.211/headList/11.jpg'
   },
   {
     id: 12,
-    url: 'http://106.13.204.211/headList/1-160430203151-50.jpg'
+    url: 'http://106.13.204.211/headList/12.jpg'
   },
   {
     id: 13,
-    url: 'http://106.13.204.211/headList/1-160430203151-51.jpg'
+    url: 'http://106.13.204.211/headList/13.jpg'
   },
   {
     id: 14,
-    url: 'http://106.13.204.211/headList/1-160430203151-53.jpg'
+    url: 'http://106.13.204.211/headList/14.jpg'
   },
+  {
+    id: 15,
+    url: 'http://106.13.204.211/headList/15.jpg'
+  },
+  {
+    id: 16,
+    url: 'http://106.13.204.211/headList/16.jpg'
+  },
+  {
+    id: 17,
+    url: 'http://106.13.204.211/headList/17.jpg'
+  },
+  {
+    id: 18,
+    url: 'http://106.13.204.211/headList/18.jpg'
+  },
+  {
+    id: 19,
+    url: 'http://106.13.204.211/headList/19.jpg'
+  },
+  {
+    id: 20,
+    url: 'http://106.13.204.211/headList/20.jpg'
+  }
 ]
